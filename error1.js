f0a695d37403c98c87280572a0a5332435bcec21
sumin-hwang <suumiin.h@gmail.com>
2023-05-27 15:22:25 +0900

    express 실습하기

--- error1.js
+++
@@ -1,9 +0,0 @@
-// try/catch로 미리 error를 잡는 예제
-setInterval(() => {
-    console.log('시작');
-    try{
-        throw new Error('break server');
-    }catch(err) {
-        console.error(err);
-    }
-}, 1000);
