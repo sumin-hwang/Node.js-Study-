c99878b55677331aaa9de068deb6fa8f30303d02
sumin-hwang <suumiin.h@gmail.com>
2023-05-28 20:08:43 +0900

    middleware test

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('cookie-parser');
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config();
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req,res) => {
-    res.send('Hello Express');
-});
+app.use(morgan('dev'));
+app.use('/', express.static(path.join(__dirname, ' public')));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave : false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie:{
+        httpOnly : true,
+        secure: false,
+    },
+    name : 'session-cookie',
+}));
+
 
 app.use((req, res, next)=> {
     console.log('모든 요청에 다 실행됩니다.');
