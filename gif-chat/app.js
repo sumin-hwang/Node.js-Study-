0b6c34a735af7ee3087289efcadbbc662a762271
sumin-hwang <suumiin.h@gmail.com>
2023-07-24 23:45:44 +0900

    웹 소켓으로 실시간 데이터 전송하기

---
+++ gif-chat/app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const nunjucks = require('nunjucks');
+const dotenv = require('dotenv');
+
+dotenv.config();
+const indexRouter = require('./routes');
+
+const app = express();
+app.set('port', process.env.PORT || 8005); // app.set('port', 포트) 서버가 실행될 포트를 설정함, 
+app.set('view engine', 'html'); // app.set(key, value) : 데이터 저장, 나중에 데이터를 app.get(key)로 가져올 수 있음
+
+nunjucks.configure('views', {
+    express : app,
+    watch : true,
+});
+
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({extended : false}));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(session({
+    resave : false,
+    saveUninitialized : false,
+    secret : process.env.COOKIE_SECRET,
+    cookie : {
+        httpOnly : true,
+        secure : false,
+    },
+}));
+
+app.use('/', indexRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status(404);
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('port'), '번 포트에서 대기 중');
+});
