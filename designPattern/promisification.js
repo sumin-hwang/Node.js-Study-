449673f845114c794af65abf5d6d584424c4d14d
sumin-hwang <suumiin.h@gmail.com>
2023-11-30 23:07:40 +0900

    콜백기반 함수 프라미스화하기

---
+++ designPattern/promisification.js
@@ -0,0 +1,17 @@
+function promisify(callbackBasedApi){
+    return function promisified(...args){
+        return new Promise((resolve, reject) => {
+            const newArgs = [
+                ...args,
+                function (err, result){
+                    if(err){
+                        return reject(err);
+                    }
+
+                    resolve(result);
+                }
+            ]
+            callbackBasedApi(...newArgs);
+        })
+    }
+}
